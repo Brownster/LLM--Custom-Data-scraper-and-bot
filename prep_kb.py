52ff162cb7b25dbe375e060fe306e87c7cfbc9b7
Marc Brown <marcsmobile.brown@googlemail.com>
2023-04-08 21:46:22 +0100

    Update prep_kb.py

--- prep_kb.py
+++ prep_kb.py
@@ -6,10 +6,40 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import TokenTextSplitter
 from langchain.document_loaders import UnstructuredURLLoader
+from typing import List
+from PyPDF2 import PdfReader
+from docx import Document
 
-os.environ["OPENAI_API_KEY"] = 'your_api_key_here'
+os.environ["OPENAI_API_KEY"] = ''
 
-def extract_links(url, level=1, max_level=3):
+def load_documents_from_directory(directory: str) -> List[str]:
+    documents = []
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if filename.endswith('.pdf'):
+            try:
+                with open(file_path, 'rb') as file:
+                    pdf_reader = PdfReader(file)
+                    text = ''
+                    for page in pdf_reader.pages:
+                        text += page.extract_text()
+                    documents.append(text)
+            except Exception as e:
+                update_status(f"Error loading document {filename}, exception: {str(e)}")
+        elif filename.endswith('.docx'):
+            try:
+                doc = Document(file_path)
+                text = "\n".join([para.text for para in doc.paragraphs])
+                documents.append(text)
+            except Exception as e:
+                update_status(f"Error loading document {filename}, exception: {str(e)}")
+        else:
+            continue
+
+    return documents
+
+
+def extract_links(url, level=1, max_level=1):
     """
     Extracts all the links from the given URL using Beautiful Soup recursively up to max_level.
     Args:
@@ -60,19 +90,21 @@ def prepare_kb():
     start_urls = urls_text.get(1.0, END).strip().split("\n")
     all_urls = []
 
+    # Get the documents directory from the GUI input fields
+    documents_directory = documents_directory_entry.get()
+    collection_name = collection_name_entry.get().lower()
+    local_directory = directory_name_entry.get()
+    max_level = int(max_level_entry.get()) if max_level_entry.get().isdigit() else 3
+    persist_directory = os.path.join(os.getcwd(), local_directory)
+
     update_status("Extracting links...")
     for url in start_urls:
-        links = extract_links(url)
+        links = extract_links(url, max_level=max_level)
         all_urls.extend(links)
         update_status(f"Extracted links from {url}")
 
     # Remove duplicates by converting the list of URLs to a set and then back to a list
     unique_urls = list(set(all_urls))
-
-    # Get the collection name and directory name from the GUI input fields
-    collection_name = collection_name_entry.get()
-    local_directory = directory_name_entry.get()
-    persist_directory = os.path.join(os.getcwd(), local_directory)
 
     # Load the documents from the unique URLs
     update_status("Loading documents from URLs...")
@@ -83,6 +115,11 @@ def prepare_kb():
     except Exception as e:
         update_status(f"Error fetching or processing URL(s), exception: {str(e)}")
         return
+
+    # Process the PDF and Word files from the directory
+    update_status("Processing local files ...")
+    documents = load_documents_from_directory(documents_directory)
+    kb_data.extend(documents)
 
     # Split the documents into smaller chunks
     text_splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -129,17 +166,30 @@ directory_label.grid(row=2, column=0, sticky=W)
 directory_name_entry = Entry(frame, width=40)
 directory_name_entry.grid(row=2, column=1, sticky=W)
 
+max_level_label = Label(frame, text="Max Link Level:")
+max_level_label.grid(row=3, column=0, sticky=W)
+
+max_level_entry = Entry(frame, width=40)
+max_level_entry.grid(row=3, column=1, sticky=W)
+
+# Add a label and entry field for the documents directory
+documents_directory_label = Label(frame, text="Documents Directory:")
+documents_directory_label.grid(row=4, column=0, sticky=W)
+
+documents_directory_entry = Entry(frame, width=40)
+documents_directory_entry.grid(row=4, column=1, sticky=W)
+
 prepare_button = Button(frame, text="Prepare", command=prepare_kb)
-prepare_button.grid(row=3, column=1, pady=(10, 0), sticky=E)
+prepare_button.grid(row=5, column=1, pady=(10, 0), sticky=E)
 
 status_label = Label(frame, text="")
-status_label.grid(row=4, column=1, pady=(10, 0), sticky=W)
+status_label.grid(row=5, column=1, pady=(10, 0), sticky=W)
 
 status_label = Label(frame, text="Status:")
-status_label.grid(row=5, column=0, pady=(10, 0), sticky=W)
+status_label.grid(row=6, column=0, pady=(10, 0), sticky=W)
 
 status_text_widget = Text(frame, wrap=WORD, width=60, height=10, state=DISABLED)
-status_text_widget.grid(row=5, column=1, pady=(10, 0), sticky=W)
+status_text_widget.grid(row=6, column=1, pady=(10, 0), sticky=W)
 
 window.mainloop()
 
